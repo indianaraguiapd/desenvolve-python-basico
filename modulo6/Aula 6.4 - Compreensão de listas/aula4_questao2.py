66e54144825d14f8aae03021705b45c9f5aa138d
indianaraguiapd <157187549+indianaraguiapd@users.noreply.github.com>
2025-05-02 16:03:16 -0300

    Add files via upload

---
+++ modulo6/Aula 6.4 - Compreensão de listas/aula4_questao2.py
@@ -0,0 +1,28 @@
+#Solicite uma frase do usuário e usando compreensão de listas imprima:
+
+#A lista de vogais da frase, ordenada alfabeticamente
+
+#A lista de consoantes da frase (remova espaços em branco)
+
+#Digite uma frase: Eu gosto de programar em Python
+
+#Vogais: ['a', 'a', 'e', 'e', 'o', 'o', 'o', 'o', 'u']
+
+#Consoantes: ['E', 'g', 's', 't', 'd', 'p', 'r', 'g', 'r', 'm', 'r', 'm', 'P', 'y', 't', 'h', 'n']
+
+
+# Solicita a frase do usuário
+frase = input("Digite uma frase: ")
+
+# Define as vogais
+vogais = 'aeiouAEIOU'
+
+# Lista de vogais ordenada alfabeticamente
+lista_vogais = sorted([char for char in frase if char in vogais])
+
+# Lista de consoantes (excluindo espaços)
+lista_consoantes = [char for char in frase if char not in vogais and char != ' ']
+
+# Imprime os resultados
+print("Vogais:", lista_vogais)
+print("Consoantes:", lista_consoantes)
